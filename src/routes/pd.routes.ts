0c22d6589403d1bda319817445f14d425d5945ed
Alejandro Rihani Salazar <rihanialex19@gmail.com>
2023-12-08 17:10:17 -0600

    abreviando las rutas de purchase_details

--- src/routes/pd.routes.ts
+++ src/routes/pd.routes.ts
@@ -6,8 +6,8 @@ const controller = new PDController(
     new PDRepository()
 );
 router.get("/purchase_details",controller.list.bind(controller));
-router.get("/purchase_details/:purchase_detailsId",controller.get.bind(controller));
+router.get("/purchase_details/:pdId",controller.get.bind(controller));
 router.post("/purchase_details",controller.create.bind(controller));
-router.put("/purchase_details/:purchase_detailsId",controller.update.bind(controller));
-router.delete("/purchase_details/:purchase_detailsId",controller.remove.bind(controller));
+router.put("/purchase_details/:pdId",controller.update.bind(controller));
+router.delete("/purchase_details/:pdId",controller.remove.bind(controller));
 export default router;
